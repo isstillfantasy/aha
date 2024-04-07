eae09f013ad14e564ba0257f80de7635ad907c49
stillfantasy <18211367+isstillfantasy@users.noreply.github.com>
2024-04-07 22:30:19 +0800

    Update 115toinfuse.js

--- 115toinfuse.js
+++ 115toinfuse.js
@@ -4,5 +4,5 @@
 1.在[Script]分组下添加下面这行配置
 http-request ^https?:\/\/.*\.115\.com\/.*\.m3u8.*$ script-path=https://raw.githubusercontent.com/ikanam/Surge-Scripts/master/115toinfuse.js
 */
-$notification.post('播放地址提取成功, 长按(重按)通知查看', '',  'infuse-' + $request.url);
+$notification.post('播放地址提取成功, 长按(重按)通知查看', '',  'infuse://play?' + $request.url);
 $done({}); 
