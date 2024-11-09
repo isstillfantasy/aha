9e9539c86fde15b049c280f6952afb28ca722a2c
stillfantasy <18211367+isstillfantasy@users.noreply.github.com>
2024-11-09 20:34:51 +0800

    Update 115toinfuse202411.js

--- 115toinfuse202411.js
+++ 115toinfuse202411.js
@@ -2,7 +2,7 @@
 提取115中的视频使用infuse进行播放.
 使用方法：
 1.在[Script]分组下添加下面这行配置
-http-request ^https?:\/\/.*\.115\.com\/.*\.m3u8.*$ script-path=https://raw.githubusercontent.com/ikanam/Surge-Scripts/master/115toinfuse.js
+http-request ^https?:\/\/.*\.115\.com\/.*\.m3u8.*$ script-path=https://raw.githubusercontent.com/isstillfantasy/aha/refs/heads/main/115toinfuse202411.js
 */
 $notification.post('播放地址提取成功, 长按(重按)通知查看', '',  'infuse://play?url=' + $request.url);
 $done({}); 
@@ -10,4 +10,3 @@ $done({});
 
 
 
-https://raw.githubusercontent.com/isstillfantasy/aha/refs/heads/main/115toinfuse20241109
